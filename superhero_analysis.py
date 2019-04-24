8bd367eabeb5619943db0ef706521bcee21b2fa6
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2019-04-24 03:49:15 -0500

    Generated the dataframe of the extracted data.

--- superhero_analysis.py
+++ superhero_analysis.py
@@ -33,33 +33,41 @@ tree = get_earnings()
 
 
 
-rank_xpath = "//tr/td[1]/font/text()"
+Rank = tree.xpath("//tr/td[1]/font/text()")
+#I am changing the data type of all the below elements of the list to int from xpath element data type.
 
-Rank = (tree.xpath(rank_xpath))
-Rank = Rank[0:-1]
-
-
-
-title_xpath = "//tr/td[2]/font/a/b/text()"
-
-Title = str(tree.xpath(title_xpath))
-
-
-studio_xpath = "//tr/td[3]/font/a/text()"
-Studio = tree.xpath(studio_xpath)
-Studio = str(Studio[1:101])
-
-
-
-gross_xpath = "//tr/td[4]/font/b/text()"
-Gross_Income = str(tree.xpath(gross_xpath))
-
-
-Gross_Income = Gross_Income.replace('$','').replace(',', '').strip()
+Rank = list(map(int, Rank[0:-1]))
 
 
 
 
+
+Title = tree.xpath("//tr/td[2]/font/a/b/text()")
+Title = list(map(str, Title))
+
+
+
+Studio = tree.xpath("//tr/td[3]/font/a/text()")
+Studio = list(map(str, Studio[1:101]))
+
+
+
+
+Gross_Income = tree.xpath("//tr/td[4]/font/b/text()")
+Gross_Income = list(map(str, Gross_Income))
+
+df = pd.DataFrame(list(zip(Rank, Title, Gross_Income, Studio)), columns=['Rank','Title', 'Gross Income','Studio'])
+
+
+
+
+df['Gross Income'] = [x.strip('$') for x in df['Gross Income']]
+df['Gross Income'] = [x.replace(',','') for x in df['Gross Income']]
+df['Gross Income'] = df['Gross Income'].astype('int64')
+
+
+
+print(df.head())
 
 
 
