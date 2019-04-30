cb7d2f68a95862c98ededddeae35a972c8155dd7
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2019-04-30 11:04:48 -0500

    Added the docstrings and the doctests.

--- superhero_analysis.py
+++ superhero_analysis.py
@@ -7,6 +7,18 @@ import numpy as np
 
 
 def get_earnings(url) -> lxml.etree:
+    """
+    The function is used to import the input url parameter to convert it into a  html document tree.
+    :param url: The url of the webpage that I want to convert into HTML document tree.
+    :return: The object returned is an lxml etree
+
+    >>> boxofficemoio_url = 'https://www.boxofficemojo.com/genres/chart/?id=superhero.htm'
+    >>> type(get_earnings(boxofficemoio_url))
+    The request was successful
+    <class 'lxml.html.HtmlElement'>
+
+    """
+
 
 
     # Fetching the url web page, and converting the response into an HTML document tree:
@@ -26,12 +38,30 @@ def get_earnings(url) -> lxml.etree:
 
 boxofficemoio_url = 'https://www.boxofficemojo.com/genres/chart/?id=superhero.htm'
 
-
-
-
 boxoffice_tree = get_earnings(boxofficemoio_url)
 
+
+wiki_url = 'https://en.wikipedia.org/wiki/List_of_highest-grossing_superhero_films'
+
+wiki_tree = get_earnings(wiki_url)
+
+#=================================================
+
+
+
 def boxoffice_data(boxoffice_tree):
+    """
+    The function is used to scrape the earnings and movie name data from the boxofficemojo website.
+    I only extracted the Title of the movie, the studio who produced it and the gross income of the movie.
+    I also removed the '$' and ',' symbols from the earnings data and also changed the data type for future analysis.
+
+    :param boxoffice_tree:  This is the
+    :return: I am returning the resultant data frame in this function.
+
+    >>> boxoffice_df = boxoffice_data(boxoffice_tree)
+    >>> print(type(boxoffice_df))
+    <class 'pandas.core.frame.DataFrame'>
+    """
 
     Title = boxoffice_tree.xpath("//tr/td[2]/font/a/b/text()")
     Title = list(map(str, Title))
@@ -53,13 +83,29 @@ def boxoffice_data(boxoffice_tree):
 boxoffice_df = boxoffice_data(boxoffice_tree)
 
 
-wiki_url = 'https://en.wikipedia.org/wiki/List_of_highest-grossing_superhero_films'
-
-wiki_tree = get_earnings(wiki_url)
+#=================================================
 
 
 
 def wiki_data(wiki_tree):
+    """
+    The function extracts the name of the movies only from the Marvel and DC comics. The previous
+    function from the boxofficemojo only extracted the name of the movies with all the different
+    production houses apart from the Marvel and DC comics.
+
+    Publishing house: Marvel, DC
+    Production house: Sony, Fox Studios, BV, Warner Bros
+
+    Marvel have contracts with different production houses thus, I would link the wiki data with the boxofficemojo
+    data in order to identify the linked publishing house.
+
+    :param wiki_tree: This returns the name of the movies only from the Marvel and DC comics.
+    :return: This returns data frame
+
+    >>> wiki_df = wiki_data(wiki_tree)
+    >>> print(type(wiki_df))
+    <class 'pandas.core.frame.DataFrame'>
+    """
     Film = []
 
     for i in list(range(2, 52)):
@@ -75,14 +121,15 @@ def wiki_data(wiki_tree):
 wiki_df = wiki_data(wiki_tree)
 
 
+
+#=================================================
+
+
 # Now I am merging the two data frames to filter the DC and Marvel only movies
 
 marvel_DC_earnings_df = boxoffice_df.merge(wiki_df, on="Title")
 
 # As "Incredibles 2" and "Teenage Mutant Ninja Turtles" are not from DC and Marvel, we need to omit them from the results.
-
-
-
 # I learnt the below code from the below link. It stores the string that we do not want in our dataframe and
 # then it removes the whole row where it finds the given string.
 # https://stackoverflow.com/questions/28679930/how-to-drop-rows-from-pandas-data-frame-that-contains-a-particular-string-in-a-p
@@ -99,11 +146,25 @@ marvel_DC_earnings_df = marvel_DC_earnings_df[~marvel_DC_earnings_df.Title.str.c
 marvel_DC_earnings_df['Publisher'] = np.where(marvel_DC_earnings_df['Studio'] == 'WB', 'DC Comics', 'Marvel Comics')
 
 
-
+#====================================================================================
 
 
 
 def fivethirtyeight_df(fte_file: str, publisher: str):
+    """
+    I am using this function to clean the data downloaded from fivethirtyeight kaggle link.
+    I am only keeping the relevant columns and cleaning the multiple column's data in order to use it for analysis.
+    I am also adding new columns in order to do the data analysis.
+
+    :param fte_file: The downloaded file that I am taking as the input.
+    :param publisher: Name of the publisher whose characters are in the mentioned data.
+    :return: It returns the cleaned and reformated data frame.
+
+    >>> dc_marvel_fte1 = fivethirtyeight_df('1fte.csv', 'DC')
+    >>> print(type(dc_marvel_fte1))
+    <class 'pandas.core.frame.DataFrame'>
+
+    """
 
     dc_fte = pd.read_csv(fte_file,
                          usecols=['name', 'ID', 'ALIGN', 'EYE', 'HAIR', 'SEX', 'ALIVE', 'APPEARANCES'])
@@ -152,26 +213,58 @@ dc_marvel_fte1 = fivethirtyeight_df('1fte.csv', 'DC')
 
 dc_marvel_fte2 = fivethirtyeight_df('2fte.csv', 'Marvel')
 
+
+# Concating the two dataframes obtained on the basis of column names.
+
+
 dc_marvel_fte_df = pd.concat([dc_marvel_fte1, dc_marvel_fte2])
 
 
+#=====================================================================================
+
 def marvel_big_data(a:str = "1c.csv", b:str = "2c.csv"):
+    """
+    Function to accept the two data sets downloaded form the Marvel_DC kaggle link and set it as default.
+    If no other parameters are entered, the default parameters would be taken as the input.
+    :param a: File 1 which contains the details about the superhero's characteristic powers
+    :param b: File 2 which contains the details about the race, height, weight, publisher
+    :return: It returns the merged data frame
+
+    >>> marvel_big_data_df = marvel_big_data()
+    >>> print(type(marvel_big_data_df))
+    <class 'pandas.core.frame.DataFrame'>
+
+    """
     df1 = pd.read_csv(a,
                       usecols=['Name', 'Intelligence', 'Strength', 'Speed',
                                'Durability', 'Power', 'Combat', 'Total'])
 
     df2 = pd.read_csv(b,
-                      usecols=['ID', 'Name', 'Alignment', 'Gender', 'Race',
-                               'Publisher', 'Height', 'Weight'])
+                      usecols=[ 'Name', 'Race',
+                                'Height', 'Weight','Publisher'])
+
 
     df3 = pd.merge(df1, df2, on='Name', how='inner')
 
-    df3['Alignment'] = df3['Alignment'].str.title()
+    df3.rename(columns = {'Name': 'Superhero Name'}, inplace=True)
+    df3 =  df3[(df3['Publisher']  == "Marvel Comics")|(df3['Publisher']  == "DC Comics") ]
+
 
     return df3
 
 
 marvel_big_data_df = marvel_big_data()
+
+#==============================================================
+
+
+
+dc_marvel_fte_df.to_csv("fte.csv")
+
+marvel_big_data_df.to_csv("big.csv")
+
+marvel_DC_earnings_df.to_csv("earnings.csv")
+
 
 
 
